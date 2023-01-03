d5c94f7639c19931b4fa9fd203ab382f83aa74b7
gwanii <lung4536@gmail.com>
2023-01-03 21:45:20 +0900

    [feat] : git action test commit - 04

--- src/main/java/shop/itbook/itbookgateway/ItbookGatewayApplication.java
+++ src/main/java/shop/itbook/itbookgateway/ItbookGatewayApplication.java
@@ -9,6 +9,7 @@ public class ItbookGatewayApplication {
 	public static void main(String[] args) {
 		System.out.println("git action test");
 		System.out.println("git action test - 02");
+		System.out.println("git action tset - 04");
 		SpringApplication.run(ItbookGatewayApplication.class, args);
 	}
 
