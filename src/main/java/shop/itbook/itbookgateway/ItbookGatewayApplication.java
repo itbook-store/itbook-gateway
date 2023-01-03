4d52e3fd0673e6197c2ab296dbd4e81babec9cdc
gwanii <lung4536@gmail.com>
2023-01-03 21:22:51 +0900

    [feat] : git action test commit

--- src/main/java/shop/itbook/itbookgateway/ItbookGatewayApplication.java
+++ src/main/java/shop/itbook/itbookgateway/ItbookGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItbookGatewayApplication {
 
 	public static void main(String[] args) {
+		System.out.println("git action test");
 		SpringApplication.run(ItbookGatewayApplication.class, args);
 	}
 
