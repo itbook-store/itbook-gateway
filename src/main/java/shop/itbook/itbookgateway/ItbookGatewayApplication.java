a797cfdcb6768019d0846ab7c04532daa9b2c0ca
gwanii <lung4536@gmail.com>
2023-01-03 21:02:49 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookgateway/ItbookGatewayApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItbookGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/itbook/itbookgateway/ItbookGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
